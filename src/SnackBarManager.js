464b3d35d85bdc0631b8c541ead675168cf474c5
Chris Mitchell <chris@translatedigital.com>
2018-04-27 12:00:59 +1200

    Changing files to fix my ocd

--- src/SnackBarManager.js
+++ src/SnackBarManager.js
@@ -1,97 +1,89 @@
 import React from 'react'
 import RootSiblings from 'react-native-root-siblings'
 
-import type { SnackItemType } from './type'
+import type {SnackItemType} from './type'
 import SnackBar from './SnackBar'
 
 export default class SnackBarManager {
-  current: SnackItemType
-  queue: Array<SnackItemType>
+    current: SnackItemType;
+    queue: Array<SnackItemType>;
 
-  constructor () {
-    this.current = null
-    this.queue = []
-  }
-
-  _setCurrent = (props: SnackItemType, callback?: Function = () => {}): void => {
-    if (!('onAutoDismiss' in props)) {
-      props.onAutoDismiss = this.dismiss
+    constructor() {
+        this.current = null;
+        this.queue = []
     }
 
-    const current = new RootSiblings(<SnackBar {...props} />)
-	this.current = current
-	callback()
-  }
+    _setCurrent = (props: SnackItemType, callback?: Function = () => {}): void => {
+        if (!('onAutoDismiss' in props)) {
+            props.onAutoDismiss = this.dismiss;
+        }
 
-  _removeCurrent = (callback?: Function = () => {}): void => {
-    if (!this.current) {
-      callback()
-      return
+        const current = new RootSiblings(<SnackBar {...props} />);
+        this.current = current;
+        callback();
+    };
+
+    _removeCurrent = (callback?: Function = () => {}): void => {
+        if (!this.current) {
+            callback()
+            return
+        }
+
+        this.current.destroy(() => {
+            this.current = null;
+            callback()
+        })
+    };
+
+    get = () => {
+        return {
+            current: this.current,
+            queue: this.queue
+        }
+    };
+
+    add = (title: string, options?: SnackItemType, callback?: Function = () => {}): void => {
+        const props = {title, ...options};
+
+        if (this.current) {
+            this.queue.push(props);
+            callback();
+            return
+        }
+
+        this._setCurrent(props, callback)
+    };
+
+    show = (title: string, options?: SnackItemType, callback?: Function = () => {}): void => {
+        const props = {title, ...options};
+
+        if (this.current) {
+            if (this._isItemAlreadyExistById(props)) {
+                return
+            }
+            this.queue.unshift(props)
+            callback();
+            return
+        }
+
+        this._setCurrent(props, callback)
+    };
+
+    dismiss = (callback?: Function = () => {}): void => {
+        this._removeCurrent(() => {
+            if (!this.queue.length) {
+                callback();
+                return
+            }
+
+            const current = this.queue.shift();
+            this._setCurrent(current, callback)
+        })
+    };
+
+    _isItemAlreadyExistById = (
+        props
+    ): boolean => {
+        return props.id && this.queue.find(item => item.id === props.id)
     }
-
-    this.current.destroy(() => {
-      this.current = null
-      callback()
-    })
-  }
-
-  get = () => {
-    return {
-      current: this.current,
-      queue: this.queue
-    }
-  }
-
-  add = (
-    title: string,
-    options?: SnackItemType,
-    callback?: Function = () => {}
-  ): void => {
-    const props = { title, ...options }
-
-    if (this.current) {
-      this.queue.push(props)
-      callback()
-      return
-    }
-
-    this._setCurrent(props, callback)
-  }
-
-  show = (
-    title: string,
-    options?: SnackItemType,
-    callback?: Function = () => {}
-  ): void => {
-    const props = { title, ...options }
-
-    if (this.current) {
-      if (this._isItemAlreadyExistById(props)) {
-        return
-      }
-      this.queue.unshift(props)
-      callback()
-      return
-    }
-
-    this._setCurrent(props, callback)
-  }
-
-  dismiss = (callback?: Function = () => {}): void => {
-    this._removeCurrent(() => {
-      if (!this.queue.length) {
-        callback()
-        return
-      }
-
-      const current = this.queue.shift()
-      this._setCurrent(current, callback)
-    })
-  }
-
-  _isItemAlreadyExistById = (
-    props
-  ): boolean => {
-    return props.id && this.queue.find(item => item.id === props.id)
-  }
 }
